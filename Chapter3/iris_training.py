b6bb8c9d8a88e6b8f621d6a644f6bb36c846fdeb
Jian Shen Yap <jianshen@dangodigital.com>
2017-09-12 05:12:05 +0800

    Chapter 3: Perceptron Algorithm with scikit-learn

---
+++ Chapter3/iris_training.py
@@ -0,0 +1,35 @@
+from sklearn import datasets
+import numpy as np
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+from sklearn.metrics import accuracy_score
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+from decision_region import plot_decision_regions
+import matplotlib.pyplot as plt
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105,150))
+plt.xlabel('petal length [standardized]') 
+plt.ylabel('petal width [standardized]') 
+plt.legend(loc='upper left')
+plt.show()
